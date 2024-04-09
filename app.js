47962686a4874ea923c0c2ec1e997b47e132e63f
MohdAnwaruddin <anwaruddinmohammad19@gmail.com>
2024-04-09 02:00:57 -0400

    renamed import to required

--- app.js
+++ app.js
@@ -22,16 +22,16 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
     cors() );
 
-app.use(helmet
-        ({
-        referrerPolicy: { policy: 'no-referrer-when-downgrade' }
-      })
-      );
+// app.use(helmet
+//         ({
+//         referrerPolicy: { policy: 'no-referrer-when-downgrade' }
+//       })
+//       );
       
-app.use(bodyParser.urlencoded({ extended: false }));
 
 
     connect().then(() => {
